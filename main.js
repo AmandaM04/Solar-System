0fb178563cff88967ab828fc17e4682431828930
Amanda Mitchell <wreedamitchell@gmail.com>
2018-03-31 12:52:54 -0500

    XHR working and printing

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // function that actually opens the single planet card = buildPlanet
 // to show the image on hover - add a different class to each planet (class=planetname(changes for each planet)image)
 // function that works with the red X = killPlanet - called inside the buildPlanet function
-//show image function - going to change the card classes inner html to an img tag
+// show image function - going to change the card classes inner html to an img tag
 // only printing a div with heading tag for search = buildDwarfPlanet
 // loop through json file and prints each dwarfplanet file through the json file
 
@@ -13,17 +13,46 @@ const printToDom = (domString, divId) => {
     document.getElementById(divId).innerHTML = domString;
 };
 
-console.log();
+const buildDomString = (planetsArray) => {
+    console.log("planetsArray", planetsArray);
+    let domString = '';
+    planetsArray.forEach((planet) => {
+      domString +=  `<div>`;
+      domString +=      `<h1>${planet.name}</h1>`;
+      domString +=      `<img class="planet-image" src="${planet.imageURL}" alt="">`;
+      domString +=      `<p>${planet.description}</p>`;
+      domString +=      `<p>${planet.isGasPlanet}</p>`;
+      domString +=      `<p>${planet.numberOfMoons}</p>`;
+      domString +=      `<p>${planet.nameOfLargestMoon}</p>`;
+      domString +=  `</div>`;
+      console.log(planet);
+    });
+    
+    printToDom(domString, 'milkyWay')
+};
 
+// buildDomString(planets);
 
+function executeThisCodeIfXHRFails(){
+    console.log("something went wrong");
+};
+
+function FileLoaded (){
+    // console.log("FileLoaded", FileLoaded);
+//     console.log("this", this);
+//     console.log("this.responseText", this.responseText);
+    const data = JSON.parse(this.responseText);
+//     console.log("data", data);
+    buildDomString(data.planets);
+};
 
 const startApplication = () => {
     let myRequest = new XMLHttpRequest();
     myRequest.addEventListener("load", FileLoaded);
     myRequest.addEventListener("error", executeThisCodeIfXHRFails);
-    myRequest.open("GET", "planets.json");
+    myRequest.open("GET", "planet.json");
     myRequest.send();
-    // console.log("myrequest", myRequest);
+    console.log("myrequest", myRequest);
   };
   
   startApplication();
