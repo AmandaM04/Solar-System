3fdb5dba8f269579b974d47ba765eae928bcac7f
Amanda Mitchell <wreedamitchell@gmail.com>
2018-04-05 22:00:52 -0500

    created single card x and search

--- main.js
+++ main.js
@@ -14,10 +14,11 @@ const printToDom = (domString, divId) => {
 };
 
 const buildDomString = (planetsArray) => {
-    console.log("planetsArray", planetsArray);
+   // console.log("planetsArray", planetsArray);
     let domString = '';
+    let i = 0;
     planetsArray.forEach((planet) => {
-      domString +=  `<div class="planet">`;
+      domString +=  `<div class="planet" id="${i}">`;
       domString +=      `<h1>${planet.name}</h1>`;
       domString +=      `<img class="hide planet-image" src="${planet.imageURL}" alt="">`;
     //   domString +=      `<p>${planet.description}</p>`;
@@ -25,8 +26,9 @@ const buildDomString = (planetsArray) => {
     //   domString +=      `<p>${planet.numberOfMoons}</p>`;
     //   domString +=      `<p>${planet.nameOfLargestMoon}</p>`;
       domString +=  `</div>`;
+    i++;
     });
-    
+   
     printToDom(domString, 'milkyWay')
     planetEvents();
 };
@@ -37,7 +39,8 @@ const planetEvents = () => {
     for (let i=0; i < planets.length; i++) {
         console.log(planets[i]);
         planets[i].addEventListener("mouseenter", hoverPlanets);
-        planets[i].addEventListener("mouseleave", showPlanets)
+        planets[i].addEventListener("mouseleave", showPlanets);
+       // planets[i].addEventListener("click", clickPlanets);
     }
 };
 
@@ -60,8 +63,41 @@ function FileLoaded (){
 //     console.log("this", this);
 //     console.log("this.responseText", this.responseText);
     const data = JSON.parse(this.responseText);
+
 //     console.log("data", data);
     buildDomString(data.planets);
+    clickPlanets(data.planets);
+    initializeSearchBar(data.planets);
+};
+
+const filterResults = (arr, searchKey) => {
+    return arr.filter((obj) => {
+      return Object.keys(obj).some((key) => {
+        if (typeof obj[key] === 'string') {
+            return obj[key].toLowerCase().includes(searchKey.toLowerCase());
+        }
+      })
+    });
+  }
+
+const searchBar = (planetArray) => {
+    const input = document.getElementById('search-field');
+        if (input.value == '') {   
+        buildDomString(planetArray);
+        clickPlanets(planetArray);
+        initializeSearchBar(planetArray);
+    } else {
+        const resultsArray = filterResults(planetArray, input.value);
+        buildDomString(resultsArray);
+        clickPlanets(resultsArray);
+    }
+};
+
+const initializeSearchBar = (planetArray) => {
+    const searchDiv = document.getElementById('search-field');
+    searchDiv.addEventListener('keypress', (e) => {
+        searchBar(planetArray);
+    });
 };
 
 const startApplication = () => {
@@ -74,3 +110,59 @@ const startApplication = () => {
   };
   
   startApplication();
+
+   const clickPlanets = (theArray) => {
+    //e.target.classList.add("hide");
+   // document.getElementById('milkyWay').classList.add('hide');
+    const planets = document.getElementsByClassName("planet");
+    const planetArray = Array.from(planets);
+    planetArray.forEach(planet => {
+        planet.addEventListener('click', (e) => {
+            buildPlanet(e, theArray);
+        });
+    });
+   };
+
+   const killPlanet = (planetArray) => {
+    let doomButton = document.getElementById('red-x');
+    doomButton.children[0].addEventListener('click', (e) => {
+        buildDomString(planetArray);
+        clickPlanets(planetArray);
+    });
+};
+
+  const buildPlanet = (e, planetArray) => {
+    let target = e.target;
+   // console.log("planet", planet);
+   let clicked = target.id;
+   console.log(clicked);
+   const redXURL = 'https://vignette.wikia.nocookie.net/mysingingmonsters/images/2/24/Red-x.png';
+    let domString = '';
+      domString +=  `<div class="planet">`;
+      domString +=      `<h1>${planetArray[clicked].name}</h1>`;
+      domString +=      `<button id="red-x"><img src="${redXURL}" width="20px"> </button>`;
+      domString +=      `<img class="planet-image" src="${planetArray[clicked].imageURL}" alt="">`;
+      domString +=      `<p>${planetArray[clicked].description}</p>`;
+      domString +=      `<p>${planetArray[clicked].isGasPlanet}</p>`;
+      domString +=      `<p>${planetArray[clicked].numberOfMoons}</p>`;
+      domString +=      `<p>${planetArray[clicked].nameOfLargestMoon}</p>`;
+      domString +=  `</div>`;
+      
+      //console.log(domString);
+      printToDom(domString, 'milkyWay')
+      killPlanet(planetArray);
+    };
+    
+
+
+
+  const startApplication2 = () => {
+    let myRequest2 = new XMLHttpRequest();
+    myRequest2.addEventListener("load", FileLoaded);
+    myRequest2.addEventListener("error", executeThisCodeIfXHRFails);
+    myRequest2.open("GET", "planet.json");
+    myRequest2.send();
+    console.log("myrequest2", myRequest2);
+  };
+  
+  
